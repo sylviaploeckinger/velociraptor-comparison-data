5130bfc6063ef871ff51cffc99a82a196b645f32
Calvin Sykes <calvin.v.sykes@durham.ac.uk>
2020-01-16 21:16:09 +0000

    add some comments

--- data/GalaxyStellarMassFunction/conversion/convertTomczak2013.py
+++ data/GalaxyStellarMassFunction/conversion/convertTomczak2013.py
@@ -8,6 +8,11 @@ import sys
 
 
 def handle_bad_values(parts):
+    """
+    Convert "missing data" values to NaNs and fix the "errors" on these values getting misinterpreted as additional data points.
+
+    parts: list of strings obtained by splitting a line from the datafile on "   "
+    """
     n_parts = len(parts)
     to_remove = []
     for i in range(n_parts):
@@ -21,13 +26,19 @@ def handle_bad_values(parts):
     return parts
 
 
-def load_file_and_split_by_z(file):
-    with open(file, "r") as f:
+def load_file_and_split_by_z(raw_file_name):
+    """
+    Read the data file and do all the mucking around needed to extract lists of the redshift and stellar mass bins for which the GSMF is tabulated, along with the corresponding GSMF values and their errors.
+
+    raw_file_name: the file name of the raw data file to extract the GSMF from 
+    """
+    with open(raw_file_name, "r") as f:
         lines = f.readlines()
     # count lines that start with comment (#) or are blank
     n_header_lines = sum(re.match("#.+|^\s*$", l) is not None for l in lines)
 
     z_ranges = lines[4]
+    # Each range of redshifts is separated by two or more space characters
     z_ranges = re.split("#?\s{2,}", z_ranges)[1:]
     z_bins_arr = np.asarray([float(z_rge.split()[0]) for z_rge in z_ranges])
 
@@ -38,16 +49,20 @@ def load_file_and_split_by_z(file):
     mass_bins_arr = np.zeros(n_stellar_mass_bins)
 
     for ism, l in enumerate(lines[n_header_lines:]):
+        # The GSMF values for each redshift bin are separated by two or more space characters
         parts = re.split("#?\s{2,}", l)
+        # The first number on each line is the stellar mass bin
         mass_bins_arr[ism] = float(parts[0])
 
         if any(p == "-99" or p == "" for p in parts):
             # this indicates "bad value" and the errors will be given as "0"
-            # we will incorrectly register these as new parts because they are separated by more than three spaces
-            # so deal with them first
+            # but we will incorrectly register them as further data values
+            # due to the way the file is formatted
+            # so we need to sort this out first
             parts = handle_bad_values(parts)
 
         for iz, part in enumerate(parts[1:]):
+            # each redshift bin has a 3-tuple of (GSMF, +ve error, -ve error)
             phi, errp, errn = map(float, part.split())
             gsmf_arr[iz, ism] = phi, errp, errn
 
@@ -55,7 +70,13 @@ def load_file_and_split_by_z(file):
 
 
 def process_for_redshift(z, mstar_bins, gsmf_at_z):
-    """Output an HDF5 file containing the GSMF at given redshift z."""
+    """
+    Output an HDF5 file containing the GSMF at a given redshift.
+
+    z: the redshift to produce the GSMF for. The given value corresponds to the lower edge of a range in redshift, which has width 0.25 for z < 1.5 and 0.5 for z >= 1.5
+    mstar_bins: the list of stellar mass bins for which the GSMF is tabulated
+    gsmf_at_z: the slice of the GSMF array at the chosen redshift
+    """
 
     processed = ObservationalData()
 
@@ -94,10 +115,15 @@ def process_for_redshift(z, mstar_bins, gsmf_at_z):
 
 
 def stringify_z(z):
-    """Eagle-style text formatting of redshift label"""
+    """
+    Eagle-style text formatting of redshift label.
+    Example: z=1.5 will be printed as z001p500.
+
+    z: The redshift to produce a label for
+    """
     whole = int(z)
     frac = int(1000 * (z - whole))
-    return f"{whole:03d}p{frac:03d}"
+    return f"z{whole:03d}p{frac:03d}"
 
 
 # Exec the master cosmology file passed as first argument
@@ -107,18 +133,17 @@ def stringify_z(z):
 with open(sys.argv[1], "r") as handle:
     exec(handle.read())
 
-input_filename = os.path.join(os.path.dirname(__file__), "../raw/Tomczak2013.txt")
+input_filename = "../raw/Tomczak2013.txt"
 
-output_filename = "Tomczak2013_z{}.hdf5"
+output_filename = "Tomczak2013_{}.hdf5"
 output_directory = "../"
 
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
 
-# Load data for all redshifts from the text file
 # z_bins, mstar_bins are 1-D ndarrays containing the lower edges of the redshift bins,
 # and the log(stellar mass) bins respectively
-# gsmf is a 3-D ndarray with axes 0 and 1 corresponding to z and Mstar
+# gsmf is a 3-D ndarray with axes 0 and 1 corresponding to z and stellar mass bins
 # Axis 2 ranges from 0..2 and contains log(GSMF), and the +- errors respectively
 z_bins, mstar_bins, gsmf = load_file_and_split_by_z(input_filename)
 
