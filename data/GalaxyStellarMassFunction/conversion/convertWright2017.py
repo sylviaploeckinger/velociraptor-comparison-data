2680a98ce2b10295d0a43a153912b7d0ee1910f1
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-01-17 07:34:10 +0000

    Formatted Wright2017

--- data/GalaxyStellarMassFunction/conversion/convertWright2017.py
+++ data/GalaxyStellarMassFunction/conversion/convertWright2017.py
@@ -25,16 +25,16 @@ if not os.path.exists(output_directory):
 processed = ObservationalData()
 raw = np.loadtxt(input_filename, delimiter=delimiter)
 
-log10_Mstar = raw[:,0]
-Phi = raw[:,1]
-Phi_minus = raw[:,2]
-Phi_plus = raw[:,3]
+log10_Mstar = raw[:, 0]
+Phi = raw[:, 1]
+Phi_minus = raw[:, 2]
+Phi_plus = raw[:, 3]
 
 # Convert to SWIFT-friendly units and correct for cosmology
-Mstar = (10. ** log10_Mstar) * unyt.Solar_Mass * (h_sim / h_obs)**-2
-Phi = Phi * unyt.Mpc ** (-3) * (h_sim / h_obs)**3
-Phi_plus = (Phi_plus * unyt.Mpc ** (-3)) * (h_sim / h_obs)**3
-Phi_minus = (Phi_minus * unyt.Mpc ** (-3)) * (h_sim / h_obs)**3
+Mstar = (10.0 ** log10_Mstar) * unyt.Solar_Mass * (h_sim / h_obs) ** -2
+Phi = Phi * unyt.Mpc ** (-3) * (h_sim / h_obs) ** 3
+Phi_plus = (Phi_plus * unyt.Mpc ** (-3)) * (h_sim / h_obs) ** 3
+Phi_minus = (Phi_minus * unyt.Mpc ** (-3)) * (h_sim / h_obs) ** 3
 
 # Meta-data
 comment = f"Data obtained assuming a Chabrier IMF and h = 0.7. h-corrected for SWIFT using cosmology: {cosmology.name}. Ignoring the mass bins for which GAMA is systematically incomplete."
@@ -45,8 +45,15 @@ redshift = 0.1
 plot_as = "points"
 
 # Write everything
-processed.associate_x(Mstar, scatter=None, comoving=True, description="Galaxy Stellar Mass")
-processed.associate_y(Phi, scatter=unyt.unyt_array([Phi_minus, Phi_plus]), comoving=True, description="Phi (GSMF)")
+processed.associate_x(
+    Mstar, scatter=None, comoving=True, description="Galaxy Stellar Mass"
+)
+processed.associate_y(
+    Phi,
+    scatter=unyt.unyt_array([Phi_minus, Phi_plus]),
+    comoving=True,
+    description="Phi (GSMF)",
+)
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
 processed.associate_comment(comment)
@@ -57,6 +64,6 @@ processed.associate_cosmology(cosmology)
 output_path = f"{output_directory}/{output_filename}"
 
 if os.path.exists(output_path):
-        os.remove(output_path)
+    os.remove(output_path)
 
 processed.write(filename=output_path)
