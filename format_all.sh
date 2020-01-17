115a10665f6dca12ebc19ad9ce065f55f5a36fd2
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-01-17 14:39:39 +0000

    Updated format script to accept --check

--- format_all.sh
+++ format_all.sh
@@ -1,4 +1,11 @@
-black cosmology.py
-black plot_individual_dataset.py
+#!bash
+# Formats all relevant source code files.
+# Accepts an extra input argument that is passed to black.
+# Useful for --check.
 
-black data/*/conversion/*.py
+extra_arg=${1}
+
+black cosmology.py $extra_arg
+black plot_individual_dataset.py $extra_arg
+
+black data/*/conversion/*.py $extra_arg
