c7d23dd49046f98e6c487764912fab1254132107
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-01-22 16:17:49 +0100

    Actually pass the scatter to the module...

--- data/GalaxyStellarMassBlackHoleMass/conversion/convertMcConnell2013_Data.py
+++ data/GalaxyStellarMassBlackHoleMass/conversion/convertMcConnell2013_Data.py
@@ -39,7 +39,7 @@ M_star = M_star[mask]
 M_star_up = (10.0 ** (np.log10(M_star / unyt.Solar_Mass) + 0.24)) * unyt.Solar_Mass
 M_star_lo = (10.0 ** (np.log10(M_star / unyt.Solar_Mass) - 0.24)) * unyt.Solar_Mass
 
-# Define the scatter as offset from the mean
+# Define the scatter as offset from the mean value
 x_scatter = (M_star - M_star_lo, M_star_up - M_star)
 y_scatter = (M_BH - M_BH_lo, M_BH_up - M_BH)
 
@@ -58,9 +58,11 @@ h = h_sim
 # Write everything
 processed = ObservationalData()
 processed.associate_x(
-    M_star, scatter=None, comoving=True, description="Galaxy Stellar Mass"
+    M_star, scatter=x_scatter, comoving=True, description="Galaxy Stellar Mass"
 )
-processed.associate_y(M_BH, scatter=None, comoving=True, description="Black Hole Mass")
+processed.associate_y(
+    M_BH, scatter=y_scatter, comoving=True, description="Black Hole Mass"
+)
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
 processed.associate_comment(comment)
