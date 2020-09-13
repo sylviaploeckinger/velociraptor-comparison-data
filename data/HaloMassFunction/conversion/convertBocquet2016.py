4962e1fc46418082267d7c1c527e3ea3f6247911
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-09-13 16:58:02 +0200

    Added script to produce halo mass function data for the EAGLE cosmology

---
+++ data/HaloMassFunction/conversion/convertBocquet2016.py
@@ -0,0 +1,53 @@
+from velociraptor.observations.objects import ObservationalData
+from velociraptor.observations.objects import ObservationalData
+
+import unyt
+import numpy as np
+import os
+import sys
+
+# Exec the master cosmology file passed as first argument
+with open(sys.argv[1], "r") as handle:
+    exec(handle.read())
+
+input_filename = "../raw/HMF_Colossus.txt"
+delimiter = "\t"
+
+output_filename = "Bocquet2016.hdf5"
+output_directory = "../"
+
+if not os.path.exists(output_directory):
+    os.mkdir(output_directory)
+
+processed = ObservationalData()
+raw = np.loadtxt(input_filename, delimiter=delimiter)
+
+M_200 = raw[:, 0]
+Phi = raw[:, 2]
+
+comment = (
+    "Halo mass functions at z=0 from the colossus package (Diemer+18) assuming a 'planck13' cosmology. "
+    "Corrected to h-free units and dn/dlog10(M) (from dn/dln(M))."
+)
+citation = "Bocquet et al. (2016) (hydro)"
+bibcode = "2016MNRAS.456.2361B"
+name = "Halo mass function fit to the Magneticum simulations"
+plot_as = "line"
+redshift = 0.0
+h = 0.6777
+
+processed.associate_x(M_200, scatter=None, comoving=True, description="Halo Mass")
+processed.associate_y(Phi, scatter=None, comoving=True, description="Phi (HMF)")
+processed.associate_citation(citation, bibcode)
+processed.associate_name(name)
+processed.associate_comment(comment)
+processed.associate_redshift(redshift)
+processed.associate_plot_as(plot_as)
+processed.associate_cosmology(cosmology)
+
+output_path = f"{output_directory}/{output_filename}"
+
+if os.path.exists(output_path):
+    os.remove(output_path)
+
+processed.write(filename=output_path)
