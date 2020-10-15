4856b0fb906261ff5ab40728079ea195198084a4
Evgenii Chaikin <chaikin@strw.leidenuniv.nl>
2020-10-15 16:49:00 +0200

    BN98 halo-mass, multiple-z data -- Switch from incorrect M_{200,crit} to correct M_BN92 -- Make the scripts produce Berhoozi2019 data at z=[0, 0.2, 0.5, 1, 2, 3, 4, 5]

--- data/GalaxyStellarMassHaloMass/conversion/convertBehroozi2019RatioStellar.py
+++ data/GalaxyStellarMassHaloMass/conversion/convertBehroozi2019RatioStellar.py
@@ -13,66 +13,69 @@ with open(sys.argv[1], "r") as handle:
 # Cosmology
 h_sim = cosmology.h
 
-output_filename = "Behroozi2019RatioStellar.hdf5"
-output_directory = "../"
+# Redshifts at which to plot the data
+redshifts = [0.0, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0]
 
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
+# Create and save data for each z in redshifts
+for z in redshifts:
 
-# Redshift at which to plot the data
-redshift = 0.0
+    output_filename = f"Behroozi2019RatioStellar_{f'z{z:.1f}'.replace('.', 'p')}.hdf5"
+    output_directory = "../"
 
-M_vir = np.logspace(9, 15, 512)
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
 
-# Note that the function below actually takes M_{vir, peak}, and we are
-# ignoring this fact
-M_star = behroozi_2019_raw(redshift, M_vir)
+    M_BN98 = np.logspace(9, 15, 512)
 
-# Correct M_vir --> M_200 (fit from EAGLE cosmology - cosmology dependence is very weak)
-M_200 = M_vir / 1.2
+    # Note that the function below actually takes M_{BN98, peak}, and we are
+    # ignoring this fact
+    M_star = behroozi_2019_raw(z, M_BN98)
 
-# Meta-data
-comment = (
-    "The data is taken from https://www.peterbehroozi.com/data.html. "
-    "Median fit to the raw data for centrals (i.e. excluding satellites). "
-    "The stellar mass is the true stellar mass (i.e. w/o observational corrections). "
-    "The halo mass is the peak halo mass that follows the Bryan & Norman (1998) "
-    "spherical overdensity definition. "
-    "The fitting function does not include the intrahalo light contribution to the "
-    "stellar mass. "
-    "Cosmology: Omega_m=0.307, Omega_lambda=0.693, h=0.678, sigma_8=0.823, n_s=0.96. "
-    "Shows the ratio between stellar mass and halo mass as a function of stellar mass."
-)
-citation = "Behroozi et al. (2019)"
-bibcode = "2019MNRAS.488.3143B"
-name = "Fit to the stellar mass - halo mass relation at z={:.1}".format(redshift)
-plot_as = "line"
-h = h_sim
+    # Meta-data
+    comment = (
+        "The data is taken from https://www.peterbehroozi.com/data.html. "
+        "Median fit to the raw data for centrals (i.e. excluding satellites). "
+        "The stellar mass is the true stellar mass (i.e. w/o observational "
+        "corrections). "
+        "The halo mass is the peak halo mass that follows the Bryan & Norman (1998) "
+        "spherical overdensity definition. "
+        "The fitting function does not include the intrahalo light contribution to the "
+        "stellar mass. "
+        "Cosmology: Omega_m=0.307, Omega_lambda=0.693, h=0.678, sigma_8=0.823, "
+        "n_s=0.96. "
+        "Shows the ratio between stellar mass and halo mass as a function of stellar "
+        "mass. "
+    )
+    citation = "Behroozi et al. (2019)"
+    bibcode = "2019MNRAS.488.3143B"
+    name = "Fit to the stellar mass - halo mass relation at z={:.1f}".format(z)
+    plot_as = "line"
+    h = h_sim
 
-# Write everything
-processed = ObservationalData()
-processed.associate_x(
-    M_star * unyt.Solar_Mass,
-    scatter=None,
-    comoving=True,
-    description="Galaxy Stellar Mass",
-)
-processed.associate_y(
-    (M_star / M_200) * unyt.dimensionless,
-    scatter=None,
-    comoving=True,
-    description="Galaxy Stellar Mass / Halo Mass ($M_* / M_{200, {\rm crit}}$)",
-)
-processed.associate_citation(citation, bibcode)
-processed.associate_name(name)
-processed.associate_comment(comment)
-processed.associate_redshift(redshift)
-processed.associate_plot_as(plot_as)
-processed.associate_cosmology(cosmology)
+    # Write everything
+    processed = ObservationalData()
+    processed.associate_x(
+        M_star * unyt.Solar_Mass,
+        scatter=None,
+        comoving=True,
+        description="Galaxy Stellar Mass",
+    )
+    processed.associate_y(
+        (M_star / M_BN98) * unyt.dimensionless,
+        scatter=None,
+        comoving=True,
+        description=r"Galaxy Stellar Mass / Halo Mass ($M_* / M_{\rm BN98}$)",
+    )
+    processed.associate_citation(citation, bibcode)
+    processed.associate_name(name)
+    processed.associate_comment(comment)
+    processed.associate_redshift(z)
+    processed.associate_plot_as(plot_as)
+    processed.associate_cosmology(cosmology)
 
-output_path = f"{output_directory}/{output_filename}"
+    output_path = f"{output_directory}/{output_filename}"
 
-if os.path.exists(output_path):
-    os.remove(output_path)
+    if os.path.exists(output_path):
+        os.remove(output_path)
 
-processed.write(filename=output_path)
+    processed.write(filename=output_path)
