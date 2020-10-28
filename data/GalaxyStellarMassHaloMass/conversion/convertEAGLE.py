3871f1eeeea97eedc56debb4059f7e9de9cdc25a
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-10-28 23:08:55 +0100

    Update the EAGLE M* - Mh conversion script

--- data/GalaxyStellarMassHaloMass/conversion/convertEAGLE.py
+++ data/GalaxyStellarMassHaloMass/conversion/convertEAGLE.py
@@ -12,55 +12,74 @@ with open(sys.argv[1], "r") as handle:
 # Cosmology
 h_sim = cosmology.h
 
-output_filename = "Schaye2015_Ref_100_z0p0.hdf5"
-output_directory = "../"
+# Redshifts at which to plot the data
+redshifts = [0.0, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0]
 
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
+haloes = ["M200", "MBN98"]
+latex_names = ["$M_{200, {\\rm crit}}$", "$M_{\\rm BN98}$"]
 
-data = np.loadtxt("../raw/EAGLE_SMHM_L100N1504_z0p0.txt")
-M_200 = data[:, 0] * unyt.Solar_Mass
-M_star_16 = data[:, 1] * unyt.Solar_Mass
-M_star_50 = data[:, 2] * unyt.Solar_Mass
-M_star_84 = data[:, 3] * unyt.Solar_Mass
+# Create and save data for each halo definition and z in redshifts
+for halo, latex_name in zip(haloes, latex_names):
+    for z in redshifts:
 
-# Define the scatter as offset from the mean value
-y_scatter = unyt.unyt_array((M_star_50 - M_star_16, M_star_84 - M_star_50))
+        output_filename = (
+            f"Schaye2015_Ref_100_{f'{halo:s}'}_{f'z{z:07.3f}'.replace('.', 'p')}.hdf5"
+        )
+        output_directory = "../"
 
-# Meta-data
-comment = (
-    "Medians obtained directly from the subfind catalogs. "
-    "Central galaxies only. Halos with 0 stellar mass included. "
-    "No cosmology correction needed."
-)
-citation = "EAGLE - L100N1504"
-bibcode = "2015MNRAS.446..521S"
-name = "Galaxy Stellar Mass - Halo Mass relation at z=0."
-plot_as = "line"
-redshift = 0.0
-h = h_sim
+        if not os.path.exists(output_directory):
+            os.mkdir(output_directory)
 
-# Write everything
-processed = ObservationalData()
-processed.associate_x(
-    M_200, scatter=None, comoving=True, description="Halo Mass ($M_{200, {\rm crit}}$)"
-)
-processed.associate_y(
-    M_star_50,
-    scatter=y_scatter,
-    comoving=True,
-    description="Galaxy Stellar Mass (30kpc, 3D)",
-)
-processed.associate_citation(citation, bibcode)
-processed.associate_name(name)
-processed.associate_comment(comment)
-processed.associate_redshift(redshift)
-processed.associate_plot_as(plot_as)
-processed.associate_cosmology(cosmology)
+        data = np.loadtxt(
+            f"../raw/EAGLE_SMHM_{f'{halo:s}'}_L100N1504_{f'z{z:03.1f}'.replace('.', 'p')}.txt"
+        )
+        M_halo = unyt.unyt_array(data[:, 0], units=unyt.Solar_Mass)
+        M_star_16 = unyt.unyt_array(data[:, 1], units=unyt.Solar_Mass)
+        M_star_50 = unyt.unyt_array(data[:, 2], units=unyt.Solar_Mass)
+        M_star_84 = unyt.unyt_array(data[:, 3], units=unyt.Solar_Mass)
 
-output_path = f"{output_directory}/{output_filename}"
+        # Define the scatter as offset from the mean value
+        y_scatter = unyt.unyt_array((M_star_50 - M_star_16, M_star_84 - M_star_50))
 
-if os.path.exists(output_path):
-    os.remove(output_path)
+        # Meta-data
+        comment = (
+            "Medians obtained directly from the subfind catalogs. "
+            "Central galaxies only. Halos with 0 stellar mass included. "
+            f"Halo mass definition is {halo:s}. "
+            "Stellar masses use a 30kpc spherical aperture. "
+            "No cosmology correction needed."
+        )
+        citation = "EAGLE - L100N1504"
+        bibcode = "2015MNRAS.446..521S"
+        name = f"Galaxy Stellar Mass - Halo Mass relation at z={f'{z:3.1f}'}."
+        plot_as = "line"
+        redshift = z
+        h = h_sim
 
-processed.write(filename=output_path)
+        # Write everything
+        processed = ObservationalData()
+        processed.associate_x(
+            M_halo,
+            scatter=None,
+            comoving=True,
+            description=f"Halo Mass ({latex_name:s})",
+        )
+        processed.associate_y(
+            M_star_50,
+            scatter=y_scatter,
+            comoving=True,
+            description="Galaxy Stellar Mass (30kpc, 3D)",
+        )
+        processed.associate_citation(citation, bibcode)
+        processed.associate_name(name)
+        processed.associate_comment(comment)
+        processed.associate_redshift(redshift)
+        processed.associate_plot_as(plot_as)
+        processed.associate_cosmology(cosmology)
+
+        output_path = f"{output_directory}/{output_filename}"
+
+        if os.path.exists(output_path):
+            os.remove(output_path)
+
+        processed.write(filename=output_path)
