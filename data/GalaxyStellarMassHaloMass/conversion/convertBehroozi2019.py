0fba19bd89893b4b68e4e8ef5afc50938c8da732
Evgenii Chaikin <chaikin@strw.leidenuniv.nl>
2020-11-13 15:16:20 +0100

    Multi-z data in a single file (Behroozi2019.py ex)

--- data/GalaxyStellarMassHaloMass/conversion/convertBehroozi2019.py
+++ data/GalaxyStellarMassHaloMass/conversion/convertBehroozi2019.py
@@ -14,67 +14,66 @@ with open(sys.argv[1], "r") as handle:
 h_sim = cosmology.h
 
 # Redshifts at which to plot the data
-redshifts = [0.0, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0]
+redshifts = [0.0, 0.2, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0]
+# Create the formatted version of the above array
+redshift_header_info = ", ".join([f"{z:.1f}" for z in redshifts])
 
-# Create and save data for each z in redshifts
-for z in redshifts:
+# Halo masses
+M_BN98 = np.logspace(9, 15, 512)
 
-    output_filename = f"Behroozi2019_{f'z{z:07.3f}'.replace('.', 'p')}.hdf5"
-    output_directory = "../"
+# Stellar masses (for each z in redshifts)
+M_star = np.array([behroozi_2019_raw(z, M_BN98) for z in redshifts])
 
-    if not os.path.exists(output_directory):
-        os.mkdir(output_directory)
+output_filename = "Behroozi2019.hdf5"
+output_directory = "../"
 
-    M_BN98 = np.logspace(9, 15, 512)
+if not os.path.exists(output_directory):
+    os.mkdir(output_directory)
 
-    # Note that the function below actually takes M_{BN98, peak}, and we are
-    # ignoring this fact
-    M_star = behroozi_2019_raw(z, M_BN98)
+# Meta-data
+comment = (
+    "The data is taken from https://www.peterbehroozi.com/data.html. "
+    "Median fit to the raw data for centrals (i.e. excluding satellites). "
+    "The stellar mass is the true stellar mass (i.e. w/o observational "
+    "corrections). "
+    "The halo mass is the peak halo mass that follows the Bryan & Norman (1998) "
+    "spherical overdensity definition. "
+    "The fitting function does not include the intrahalo light contribution to the "
+    "stellar mass. "
+    "Cosmology: Omega_m=0.307, Omega_lambda=0.693, h=0.678, sigma_8=0.823, "
+    "n_s=0.96. "
+    "Shows the stellar mass as a function halo mass."
+)
+citation = "Behroozi et al. (2019)"
+bibcode = "2019MNRAS.488.3143B"
+name = f"Fit to the stellar mass - halo mass relation at z=[{redshift_header_info:s}]"
+plot_as = "line"
+h = h_sim
 
-    # Meta-data
-    comment = (
-        "The data is taken from https://www.peterbehroozi.com/data.html. "
-        "Median fit to the raw data for centrals (i.e. excluding satellites). "
-        "The stellar mass is the true stellar mass (i.e. w/o observational "
-        "corrections). "
-        "The halo mass is the peak halo mass that follows the Bryan & Norman (1998) "
-        "spherical overdensity definition. "
-        "The fitting function does not include the intrahalo light contribution to the "
-        "stellar mass. "
-        "Cosmology: Omega_m=0.307, Omega_lambda=0.693, h=0.678, sigma_8=0.823, "
-        "n_s=0.96. "
-        "Shows the stellar mass as a function halo mass."
-    )
-    citation = "Behroozi et al. (2019)"
-    bibcode = "2019MNRAS.488.3143B"
-    name = "Fit to the stellar mass - halo mass relation at z={:.1f}".format(z)
-    plot_as = "line"
-    h = h_sim
+# Write everything
+processed = ObservationalData()
+processed.associate_x(
+    M_BN98 * unyt.Solar_Mass,
+    scatter=None,
+    comoving=True,
+    description="Halo Mass ($M_{\\rm BN98}$)",
+)
+processed.associate_y(
+    M_star * unyt.Solar_Mass,
+    scatter=None,
+    comoving=True,
+    description="Galaxy Stellar Mass",
+)
+processed.associate_citation(citation, bibcode)
+processed.associate_name(name)
+processed.associate_comment(comment)
+processed.associate_redshift(redshifts)
+processed.associate_plot_as(plot_as)
+processed.associate_cosmology(cosmology)
 
-    # Write everything
-    processed = ObservationalData()
-    processed.associate_x(
-        M_BN98 * unyt.Solar_Mass,
-        scatter=None,
-        comoving=True,
-        description="Halo Mass ($M_{\\rm BN98}$)",
-    )
-    processed.associate_y(
-        M_star * unyt.Solar_Mass,
-        scatter=None,
-        comoving=True,
-        description="Galaxy Stellar Mass",
-    )
-    processed.associate_citation(citation, bibcode)
-    processed.associate_name(name)
-    processed.associate_comment(comment)
-    processed.associate_redshift(z)
-    processed.associate_plot_as(plot_as)
-    processed.associate_cosmology(cosmology)
+output_path = f"{output_directory}/{output_filename}"
 
-    output_path = f"{output_directory}/{output_filename}"
+if os.path.exists(output_path):
+    os.remove(output_path)
 
-    if os.path.exists(output_path):
-        os.remove(output_path)
-
-    processed.write(filename=output_path)
+processed.write(filename=output_path)
