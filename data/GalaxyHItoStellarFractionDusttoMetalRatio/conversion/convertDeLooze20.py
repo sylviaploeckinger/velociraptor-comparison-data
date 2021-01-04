2d1fec4db6eb0cb58faa041f1e1fbf5f9fbbc710
James <j.w.trayford@durham.ac.uk>
2021-01-04 14:45:24 +0000

    Add new data for dust property comparisions

---
+++ data/GalaxyHItoStellarFractionDusttoMetalRatio/conversion/convertDeLooze20.py
@@ -0,0 +1,168 @@
+from velociraptor.observations.objects import ObservationalData
+
+import unyt
+import numpy as np
+import os
+import sys
+import copy
+import re
+from scipy.stats import binned_statistic
+
+# Exec the master cosmology file passed as first argument
+with open(sys.argv[1], "r") as handle:
+    exec(handle.read())
+
+input_filename = "../raw/JINGLE_MHIMstar_vs_MdustMmetals.dat"
+delimiter = " "
+
+output_directory = "../"
+
+if not os.path.exists(output_directory):
+    os.mkdir(output_directory)
+
+lines = []
+labels = []
+inc = 0
+
+# find all the sub headers in the file (tthey contain a ':')
+for line in open(input_filename):
+    if re.findall(r"\:", line):
+        # store sub-head lines and survey names
+        lines.append(inc)
+        labels.append(line.split(":")[0])
+    inc += 1
+lines.append(inc)
+
+x_all = []
+y_all = []
+
+for i in range(len(labels)):
+    # read each surveys data and write separate files
+    raw = np.genfromtxt(
+        input_filename,
+        skip_header=lines[i] + 1,
+        skip_footer=inc - lines[i + 1],
+        dtype=float,
+        usecols=[1, 2, 3, 4, 5, 6],
+    )
+    label = "_".join(labels[i].split(" "))
+    output_filename = f"DeLooze20_individual_{label}.hdf5"
+
+    y_all.append(pow(10, raw[:, 0]))
+    x_all.append(pow(10, raw[:, 1]))
+
+    logMHIMstar_med = pow(10, raw[:, 0]) * unyt.dimensionless
+    logMHIMstar_lo = pow(10, raw[:, 2]) * unyt.dimensionless
+    logMHIMstar_hi = pow(10, raw[:, 4]) * unyt.dimensionless
+    logD2Z_med = pow(10, raw[:, 1]) * unyt.dimensionless
+    logD2Z_lo = pow(10, raw[:, 3]) * unyt.dimensionless
+    logD2Z_hi = pow(10, raw[:, 5]) * unyt.dimensionless
+
+    # Define the scatter as offset from the mean value
+    y_scatter = unyt.unyt_array(
+        (logMHIMstar_med - logMHIMstar_lo, logMHIMstar_hi - logMHIMstar_med)
+    )
+    x_scatter = unyt.unyt_array((logD2Z_med - logD2Z_lo, logD2Z_hi - logD2Z_med))
+
+    # Meta-data
+    comment = f"values for individual galaxies derived from {label} survey"
+    citation = f"{label} compiled by De Looze et al. (2020)"
+    bibcode = "2020MNRAS.496.3668D"
+    name = "MHI/Mstar as a function of dust-to-metal ratio"
+    plot_as = "points"
+    redshift = 0.0
+    redshift_lower = 0.0
+    redshift_upper = 3.0
+    h = 0.7
+
+    # Write everything
+    outobj = ObservationalData()
+    outobj.associate_x(
+        logD2Z_med,
+        scatter=x_scatter,
+        comoving=True,
+        description="Gas phase dust-to-metal ratio",
+    )
+    outobj.associate_y(
+        logMHIMstar_med,
+        scatter=y_scatter,
+        comoving=True,
+        description="HI mass to stellar mass ratio",
+    )
+    outobj.associate_citation(citation, bibcode)
+    outobj.associate_name(name)
+    outobj.associate_comment(comment)
+    outobj.associate_redshift(redshift, redshift_lower, redshift_upper)
+    outobj.associate_plot_as(plot_as)
+    outobj.associate_cosmology(cosmology)
+
+    output_path = f"{output_directory}/{output_filename}"
+
+    if os.path.exists(output_path):
+        os.remove(output_path)
+
+    outobj.write(filename=output_path)
+
+# Also output median and scatter points for composite sample
+x_all = np.hstack(x_all) * unyt.dimensionless
+y_all = np.hstack(y_all) * unyt.dimensionless
+
+plussig = lambda a: np.percentile(a, 84)
+subsig = lambda a: np.percentile(a, 16)
+
+x_bins = np.percentile(x_all, np.linspace(1, 99, 10)) * unyt.dimensionless
+x_mids = x_bins[:-1] + np.diff(x_bins) * 0.5
+
+y_med = (
+    binned_statistic(x_all, y_all, statistic="median", bins=x_bins)[0]
+    * unyt.dimensionless
+)
+y_psig = (
+    binned_statistic(x_all, y_all, statistic=plussig, bins=x_bins)[0]
+    * unyt.dimensionless
+)
+y_ssig = (
+    binned_statistic(x_all, y_all, statistic=subsig, bins=x_bins)[0]
+    * unyt.dimensionless
+)
+
+y_scatter = unyt.unyt_array((y_med - y_ssig, y_psig - y_med))
+x_scatter = unyt.unyt_array([np.diff(x_bins) * 0.5] * 2)
+
+output_filename = "DeLooze20_composite_median.hdf5"
+
+# Meta-data
+comment = f"Median and scatter compiled from multiple surveys"
+citation = f"Compiled by De Looze et al. (2020)"
+bibcode = "2020MNRAS.496.3668D"
+name = "MHI/Mstar as a function of dust-to-metal ratio"
+plot_as = "points"
+redshift = 0.0
+redshift_lower = 0.0
+redshift_upper = 3.0
+h = 0.7
+
+# Write everything
+outobj = ObservationalData()
+outobj.associate_x(
+    x_mids,
+    scatter=x_scatter,
+    comoving=True,
+    description="Gas phase dust-to-metal ratio",
+)
+outobj.associate_y(
+    y_med, scatter=y_scatter, comoving=True, description="HI mass to stellar mass ratio"
+)
+outobj.associate_citation(citation, bibcode)
+outobj.associate_name(name)
+outobj.associate_comment(comment)
+outobj.associate_redshift(redshift, redshift_lower, redshift_upper)
+outobj.associate_plot_as(plot_as)
+outobj.associate_cosmology(cosmology)
+
+output_path = f"{output_directory}/{output_filename}"
+
+if os.path.exists(output_path):
+    os.remove(output_path)
+
+outobj.write(filename=output_path)
