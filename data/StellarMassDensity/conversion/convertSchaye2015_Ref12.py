e1dcfd1b1e44c7d96ab92752a31f67d60fc27ac6
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-01-14 22:12:00 +0100

    Add the stellar mass density evolution from the EAGLE-12 box for COLIBRE comparison

--- data/StellarMassDensity/conversion/convertSchaye2015_Ref12.py
+++ data/StellarMassDensity/conversion/convertSchaye2015_Ref12.py
@@ -12,10 +12,10 @@ with open(sys.argv[1], "r") as handle:
 # Cosmologies
 h_sim = cosmology.h
 
-input_filename = "../raw/MassEvolution_EAGLE_Ref_100.dat"
+input_filename = "../raw/MassEvolution_EAGLE_Ref_12.dat"
 delimiter = "\t"
 
-output_filename = "Schaye2015.hdf5"
+output_filename = "Schaye2015_Ref12.hdf5"
 output_directory = "../"
 
 if not os.path.exists(output_directory):
@@ -31,16 +31,16 @@ a = unyt.unyt_array(1 / (1 + z), "dimensionless")
 
 # Read densities
 rho_star = unyt.unyt_array(raw[:, 3], "Msun")
-rho_star /= unyt.unyt_quantity(100.0, "Mpc") ** 3
+rho_star /= unyt.unyt_quantity(12.5, "Mpc") ** 3
 
 # Meta-data
 comment = (
     "Data obtained summing up all the star particles in the snapshots. "
     "No cosmology correction needed."
 )
-citation = "Schaye et al. (2015) (EAGLE)"
+citation = "Schaye et al. (2015) (EAGLE-12)"
 bibcode = "2015MNRAS..446..521S"
-name = "Stellar mass density obtained from the Ref EAGLE model"
+name = "Stellar mass density obtained from the Ref EAGLE model (12.5 Mpc volume)"
 redshift = np.mean(z)
 plot_as = "points"
 
