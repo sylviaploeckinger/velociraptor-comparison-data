9a284c5b03a7ba44fdaacd7b7c3a1ca1ed84e5df
Camila Correa <correa@strw.leidenuniv.nl>
2021-02-01 20:49:43 +0100

    formating..

--- data/GalaxyStellarMassGasMetallicity/conversion/convertChartab2021.py
+++ data/GalaxyStellarMassGasMetallicity/conversion/convertChartab2021.py
@@ -1,4 +1,7 @@
-from velociraptor.observations.objects import (ObservationalData, MultiRedshiftObservationalData,)
+from velociraptor.observations.objects import (
+    ObservationalData,
+    MultiRedshiftObservationalData,
+)
 
 import unyt
 import numpy as np
@@ -15,9 +18,9 @@ input_redshifts = [1.5, 2.3]
 
 # Meta-data
 comment = (
-           "Fits to stacks obtained assuming a Chabrier IMF. "
-           "The metallicity is expressed as 12 + log10(O/H). "
-           )
+    "Fits to stacks obtained assuming a Chabrier IMF. "
+    "The metallicity is expressed as 12 + log10(O/H). "
+)
 citation = "Chartab et al. (2021)"
 bibcode = "https://arxiv.org/pdf/2101.01706.pdf"
 name = "MOSDEF Survey: gas-phase metallicity of galaxies at 1.4 <= z <= 2.6"
@@ -40,35 +43,39 @@ for filename, redshifts in zip(input_filenames, input_redshifts):
     plot_as = "line"
     redshift = redshifts
     h = cosmology.h
-    
+
     log10_M_star_min = 9.5
     log10_M_star_max = 11.5
-    
+
     if redshift == 1.5:
-    
+
         Z_asm = 6.29
         alpha = 0.21
         M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
         Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
-        M_star = 10**M_star * unyt.Solar_Mass
-        
+        M_star = 10 ** M_star * unyt.Solar_Mass
+
     if redshift == 2.3:
-        
+
         Z_asm = 4.84
         alpha = 0.35
         M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
         Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
-        M_star = 10**M_star * unyt.Solar_Mass
+        M_star = 10 ** M_star * unyt.Solar_Mass
 
-    processed.associate_x(M_star, scatter=None, comoving=True, description="Galaxy Stellar Mass")
-    processed.associate_y(Z_gas, scatter=None, comoving=True, description="Gas phase metallicity")
+    processed.associate_x(
+        M_star, scatter=None, comoving=True, description="Galaxy Stellar Mass"
+    )
+    processed.associate_y(
+        Z_gas, scatter=None, comoving=True, description="Gas phase metallicity"
+    )
     processed.associate_citation(citation, bibcode)
     processed.associate_name(name)
     processed.associate_comment(comment)
     processed.associate_redshift(redshift)
     processed.associate_plot_as(plot_as)
     processed.associate_cosmology(cosmology)
-        
+
     multi_z.associate_dataset(processed)
 
 output_path = f"{output_directory}/{output_filename}"
@@ -77,4 +84,3 @@ if os.path.exists(output_path):
     os.remove(output_path)
 
 multi_z.write(filename=output_path)
-
