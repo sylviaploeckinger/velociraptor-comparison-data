f29ed48e8df0d1cfa6f840ffe25b20bcd95680a0
Camila Correa <correa@strw.leidenuniv.nl>
2021-02-02 11:11:51 +0100

    Changing to use MultiRedshiftObservationalData

--- data/GalaxyStellarMassGasMetallicity/conversion/convertChartab2021.py
+++ data/GalaxyStellarMassGasMetallicity/conversion/convertChartab2021.py
@@ -1,4 +1,7 @@
-from velociraptor.observations.objects import ObservationalData
+from velociraptor.observations.objects import (
+   ObservationalData,
+   MultiRedshiftObservationalData,
+)
 
 import unyt
 import numpy as np
@@ -6,89 +9,98 @@ import os
 import sys
 
 
-def stringify_z(z):
-    """
-    Eagle-style text formatting of redshift label.
-    Example: z=1.5 will be printed as z001p500.
+def StellarMass_vs_GasMetallicity():
 
-    z: The redshift to produce a label for
-    """
-    whole = int(z)
-    frac = int(1000 * (z - whole))
-    return f"z{whole:03d}p{frac:03d}"
-
-
-def process_for_redshift(z):
-    """
-    Output an HDF5 file containing the stellar mass-gas metallicity relation at a given redshift.
-
-    z: the redshift to produce the relation
-    """
-
-    processed = ObservationalData()
-
-    # Meta-data
+    name = (
+        f"Fit to the stellar mass - gas metallicity at z=[{redshift_header_info:s}]"
+    )
     comment = (
-        "Fits to stacks obtained assuming a Chabrier IMF. "
-        "The metallicity is expressed as 12 + log10(O/H). "
+               "The data is taken from Chartab+21 "
+               "Median fit to galaxy stacks from MOSDEF survey. "
+               "Stellar masses obtained assuming a Chabrier IMF. "
+               "The metallicity is expressed as 12 + log10(O/H). "
     )
-    citation = "Chartab et al. (2021)"
-    bibcode = "https://arxiv.org/pdf/2101.01706.pdf"
-    name = "MOSDEF Survey: gas-phase metallicity of galaxies at 1.4 <= z <= 2.6"
-    plot_as = "line"
 
-    redshift = z
-    h = cosmology.h
+    # Store metadata at the top level
+    multi_z = MultiRedshiftObservationalData()
+    multi_z.associate_citation(citation, bibcode)
+    multi_z.associate_name(name)
+    multi_z.associate_comment(comment)
+    multi_z.associate_cosmology(cosmology)
+    multi_z.associate_maximum_number_of_returns(1)
+    
+    output_filename = "Chartab2021.hdf5"
+    output_directory = "../"
+    
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
 
-    log10_M_star_min = 9.5
-    log10_M_star_max = 11.5
+    for z, dz_lower, dz_upper in zip(redshifts, redshifts_lower, redshifts_upper):
+    
+        # Create a single observational-data instance at redshift z
+        processed = ObservationalData()
 
-    if redshift == 1.5:
+        # Compute \Delta z
+        redshift_lower, redshift_upper = [z - dz_lower, z + dz_upper]
 
-        Z_asm = 6.29
-        alpha = 0.21
-        M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
-        Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
-        M_star = 10 ** M_star * unyt.Solar_Mass
+        log10_M_star_min = 9.5
+        log10_M_star_max = 11.5
+    
+        if z == 1.5:
+            
+            Z_asm = 6.29
+            alpha = 0.21
+            M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
+            Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
+            M_star = 10 ** M_star * unyt.Solar_Mass
+        
+        if z == 2 or z==2.5:
+            
+            Z_asm = 4.84
+            alpha = 0.35
+            M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
+            Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
+            M_star = 10 ** M_star * unyt.Solar_Mass
 
-    if redshift == 2.3:
+        processed.associate_x(M_star, scatter=None, comoving=True, description="Galaxy Stellar Mass")
+        processed.associate_y(Z_gas, scatter=None, comoving=True, description="Gas phase metallicity")
+        processed.associate_redshift(z, redshift_lower, redshift_upper)
+        processed.associate_plot_as(plot_as)
+        multi_z.associate_dataset(processed)
 
-        Z_asm = 4.84
-        alpha = 0.35
-        M_star = np.arange(log10_M_star_min, log10_M_star_max, 0.2)
-        Z_gas = (Z_asm + alpha * M_star) * unyt.dimensionless  # 12 + log(O/H)
-        M_star = 10 ** M_star * unyt.Solar_Mass
+    output_path = f"{output_directory}/{output_filename}"
 
-    processed.associate_x(
-        M_star, scatter=None, comoving=True, description="Galaxy Stellar Mass"
-    )
-    processed.associate_y(
-        Z_gas, scatter=None, comoving=True, description="Gas phase metallicity"
-    )
-    processed.associate_citation(citation, bibcode)
-    processed.associate_name(name)
-    processed.associate_comment(comment)
-    processed.associate_redshift(redshift)
-    processed.associate_plot_as(plot_as)
-    processed.associate_cosmology(cosmology)
-    return processed
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    
+    multi_z.write(filename=output_path)
 
 
 # Exec the master cosmology file passed as first argument
 with open(sys.argv[1], "r") as handle:
     exec(handle.read())
 
-output_filename = "Chartab2021_{}.hdf5"
-output_directory = "../"
-input_redshifts = (1.5, 2.3)
+# Redshifts at which to plot the data
+redshifts = np.array([1.5, 2.0, 2.5])
 
-for z in input_redshifts:
+# Valid redshift ranges for each z from above
+Delta_z = 0.5 * (redshifts[1:] - redshifts[:-1])
+redshifts_lower = np.append(0.10, Delta_z)
+redshifts_upper = np.append(Delta_z, 0.25)
 
-    processed = process_for_redshift(z)
+# Create the formatted version of the above array
+redshift_header_info = ", ".join([f"{z:.1f}" for z in redshifts])
 
-    output_path = f"{output_directory}/{output_filename.format(stringify_z(z))}"
+# Cosmology
+h_sim = cosmology.h
 
-    if os.path.exists(output_path):
-        os.remove(output_path)
+# Meta-data
+citation = "Chartab et al. (2021)"
+bibcode = "2021arXiv210101706C"
+name = "MOSDEF Survey: gas-phase metallicity of galaxies at 1.4 <= z <= 2.6"
+plot_as = "line"
+h = h_sim
 
-    processed.write(filename=output_path)
+# Create, format, and save the data
+StellarMass_vs_GasMetallicity()
+
