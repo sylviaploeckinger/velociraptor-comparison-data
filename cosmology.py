8b6a578d7a564b74f3d7ba16cfaa13c34f874921
Calvin Sykes <sykescalvin09@gmail.com>
2021-02-22 09:56:08 +0000

    imf factors again: use the same values as Furlong+ 2015

--- cosmology.py
+++ cosmology.py
@@ -2,6 +2,6 @@ from astropy.cosmology import Planck15 as cosmology
 
 solar_metallicity = 0.0126
 
-# IMF conversion factors from Speagle et al 2014
-kroupa_to_chabrier_mass = 0.943
-salpeter_to_chabrier_mass = 0.585
+# IMF conversion factors from Furlong+ 2015
+kroupa_to_chabrier_mass = 0.912
+salpeter_to_chabrier_mass = 0.606
