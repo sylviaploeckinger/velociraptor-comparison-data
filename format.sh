acde1db549ab1bf02f10090f94dca22a393fdf99
Bert Vandenbroucke <vandenbroucke@strw.leidenuniv.nl>
2022-04-14 13:34:52 +0200

    Fixed black formatting by setting up virtual environment with a pinned version of black and click.

--- format.sh
+++ format.sh
@@ -1,20 +1,39 @@
-#!bash
+#!/bin/bash
 # Formats all relevant source code files.
 # Accepts an extra input argument that is passed to black.
 # Useful for --check.
+
+# Check if we can run pip
+# This also serves as a check for python3
+python3 -m pip --version > /dev/null
+if [[ $? -ne 0 ]]
+then
+  echo "ERROR: cannot run 'python3 -m pip'"
+  exit 1
+fi
+
+# Check if the virtual environment with black exists
+if [ ! -d black_formatting_env ]
+then
+  echo "Formatting environment not found, installing it..."
+  python3 -m venv black_formatting_env
+  ./black_formatting_env/bin/python3 -m pip install click==8.0.4 black==21.12b0
+fi
+# Now we know exactly which black to use
+black="./black_formatting_env/bin/python3 -m black"
 
 extra_arg=${1}
 
 return_code=0
 
-black cosmology.py $extra_arg
+$black cosmology.py $extra_arg
 return_code=$(( $return_code > $? ? $return_code : $? ))
-black plot_individual_dataset.py $extra_arg
+$black plot_individual_dataset.py $extra_arg
 return_code=$(( $return_code > $? ? $return_code : $? ))
-black convert.py $extra_arg
+$black convert.py $extra_arg
 return_code=$(( $return_code > $? ? $return_code : $? ))
 
-black data/*/conversion/*.py $extra_arg
+$black data/*/conversion/*.py $extra_arg
 return_code=$(( $return_code > $? ? $return_code : $? ))
 
 exit $return_code
