b8d9d2ad4ce81f0fde8ab593cf9e65407e59c00e
fonotec <fonotec@hotmail.com>
2022-07-20 09:49:36 +0200

    Update the Neutral Schruba plot

--- data/AzimuthallyAveragedNeutralKSRelation/conversion/convertSchruba2011.py
+++ data/AzimuthallyAveragedNeutralKSRelation/conversion/convertSchruba2011.py
@@ -5,32 +5,18 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
-def bin_data_general(array_x, array_y, array_x_bin, x_limit, print_stuff=False):
-    # create a SFR value array
-    y_array_bin = np.zeros(len(array_x_bin)-1)
-    y_array_bin_std_up = np.zeros(len(array_x_bin)-1)
-    y_array_bin_std_down = np.zeros(len(array_x_bin)-1)
+def bin_data_general(array_x, array_y, array_x_bin):
+    array_x_bin_centres = 0.5 * (array_x_bin[1:] + array_x_bin[:-1])
+    y_array_bin, _, _ = stats.binned_statistic(array_x, array_y, statistic="median", bins=array_x_bin)
+    y_array_bin_std_up, _, _ = stats.binned_statistic(array_x, array_y, statistic=lambda x: np.percentile(x, 84.), bins=array_x_bin)
+    y_array_bin_std_down, _, _ = stats.binned_statistic(array_x, array_y, statistic=lambda x: np.percentile(x, 16.), bins=array_x_bin)
 
-    for i in range(0,len(array_x_bin)-1):
-        mask = (array_x > array_x_bin[i]) & (array_x < array_x_bin[i+1])
-        y_array_bin[i] = np.nanmedian(array_y[mask])
-        if print_stuff:
-            print(array_x_bin[i])
-            print(array_y[mask])
-            print(stats.describe(array_y[mask]))
-        try:
-            y_array_bin_std_up[i], y_array_bin_std_down[i] = np.transpose(np.percentile(array_y[mask], [16,84]))
-        except:
-            y_array_bin_std_up[i], y_array_bin_std_down[i] = [0., 0.]
+    y_array_bin_std_up = y_array_bin_std_up - y_array_bin
+    y_array_bin_std_down = y_array_bin - y_array_bin_std_down
 
-    array_x_bin =  (array_x_bin[1:] + array_x_bin[:-1])/2.
-    y_array_bin_std_up = np.abs(y_array_bin_std_up - y_array_bin)
-    y_array_bin_std_down = np.abs(y_array_bin_std_down - y_array_bin)
-    mask = array_x_bin > x_limit
-
-    return array_x_bin[mask], y_array_bin[mask], y_array_bin_std_up[mask], y_array_bin_std_down[mask]
-
+    return array_x_bin_centres, y_array_bin, y_array_bin_std_down, y_array_bin_std_up
 
 # Exec the master cosmology file passed as first argument
 with open(sys.argv[1], "r") as handle:
@@ -43,13 +29,16 @@ processed = ObservationalData()
 comment = "Based on the IRAM HERCULES survey."
 citation = "Schruba et al. (2011)"
 bibcode = "2011AJ....142...37S"
-name = "azimuthally-averaged H2 + HI Gas Surface Density vs Star Formation Rate Surface Density"
+name = "azimuthally-averaged $\\Sigma_{\\rm HI + H_2}$ vs $\\Sigma_{\\rm SFR}$"
 plot_as = "points"
 
 sigma_SFR, sigma_SFR_err, sigma_HI, sigma_HI_err, sigma_H2, sigma_H2_err = np.genfromtxt(input_filename, unpack=True, usecols=(3,4,5,6,7,8))
 
 array_of_interest = np.arange(-1,3,0.25)
 minimum_surface_density = 0.0 # The paper says 1 Msun/pc^2 is the limit in molecular gas, limit in Sigma_SFR is not yet reached at that surface density.
+array_of_interest = array_of_interest[array_of_interest>=minimum_surface_density]
+if array_of_interest[0] > minimum_surface_density:
+    array_of_interest = np.append([minimum_surface_density], array_of_interest)
 
 Obs_HI = (sigma_HI)/1.36 # a factor of 1.36 to account for heavy elements
 Obs_H2 = (sigma_H2)/1.36 # a factor of 1.36 to account for heavy elements
@@ -58,7 +47,7 @@ Obs_Hneutral = Obs_HI + Obs_H2
 
 Obs_SFR = sigma_SFR
 
-binned_data = bin_data_general(np.log10(Obs_Hneutral), np.log10(Obs_SFR), array_of_interest, minimum_surface_density)
+binned_data = bin_data_general(np.log10(Obs_Hneutral), np.log10(Obs_SFR), array_of_interest)
 
 SigmaHneutral = unyt.unyt_array(10**binned_data[0], units="Msun/pc**2")
 
@@ -66,9 +55,14 @@ SigmaSFR = unyt.unyt_array(10**binned_data[1], units="Msun/yr/kpc**2")
 
 SigmaSFR_err = unyt.unyt_array([np.abs(10**(binned_data[1]) - 10**(binned_data[1]+binned_data[2])), np.abs(10**(binned_data[1]-binned_data[3]) -10**(binned_data[1]))], units="Msun/yr/kpc**2")
 
-processed.associate_x(SigmaHneutral, scatter=None, comoving=False, description="H2 + HI Surface density")
+array_x_bin_std_up = array_of_interest[1:] - binned_data[0]
+array_x_bin_std_down = binned_data[0] - array_of_interest[:-1]
 
-processed.associate_y(SigmaSFR, scatter=SigmaSFR_err, comoving=False, description="Star Formation Rate Surface Density")
+SigmaHneutral_err = unyt.unyt_array([10**(binned_data[0])-10**(binned_data[0]-array_x_bin_std_down), 10**(binned_data[0]+array_x_bin_std_up)-10**(binned_data[0])], units="Msun/pc**2")
+
+processed.associate_x(SigmaHneutral, scatter=SigmaHneutral_err, comoving=False, description="$\\Sigma_{\\rm HI + H_2}$")
+
+processed.associate_y(SigmaSFR, scatter=SigmaSFR_err, comoving=False, description="$\\Sigma_{\\rm SFR}$")
 
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
